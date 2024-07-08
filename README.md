3ff9328f90d308f071e294d2c83961c635d8e07c
Feruza <ergashevaferuzabonuu@gmail.com>
2024-07-08 16:01:35 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# chat_app
+
+
+
+
+https://github.com/SaidazimovToxir/chat-app/assets/107400871/6f691c29-e9ca-4bab-96e5-fce8c52a8ad1
+
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:chat_app/views/screens/auth_screen.dart';
+import 'package:chat_app/views/screens/home_screen.dart';
+import 'package:chat_app/views/screens/main_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  runApp(MyApp(prefs: prefs));
+}
+
+class MyApp extends StatelessWidget {
+  final SharedPreferences prefs;
+
+  MyApp({super.key, required this.prefs});
+
+  final _firebaseFirestore = FirebaseFirestore.instance;
+  final _firebaseStorage = FirebaseStorage.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (_, AsyncSnapshot<User?> userSnapshot) {
+          print(userSnapshot);
+          if (userSnapshot.hasData) {
+            return const MainScreen();
+          }
+          return const AuthScreen();
+        },
+      ),
+    );
+  }
+}
